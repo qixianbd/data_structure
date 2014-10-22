19272541037a2c92cee5b0724e554d99487606ac
qixianbd <keymingstar@gmail.com>
2014-10-22 23:16:59 +0800

    implement class BinarySearchTree

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -16,13 +16,29 @@ public:
 	BinarySearchTree();
 	BinarySearchTree(const BinarySearchTree& bst);
 	~BinarySearchTree();
+	const BinarySearchTree& operator=(const BinarySearchTree& bst);
+
+	void insert(const T& elem);
+	bool isEmpty()const{
+		return NULL == root;
+	}
+	bool contains(const T& elem)const{
+		return contains(elem, root);
+	}
 
 private:
 	struct Node{
 		T data;
 		Node *lchild;
 		Node *rchild;
+		Node(const T& value, const Node* l, const Node* r):data(value), lchild(l), rchild(r){
+
+		}
 	};
+	Node* root;
+	Comp cmp;
+	bool contains(const T& elem, Node* root)const;
+	void insert(const T& elem, Node* root);
 };
 
 
--- detail_header/bstImpl.h
+++ detail_header/bstImpl.h
@@ -10,16 +10,67 @@
 
 
 template<typename T, typename Comp >
-inline BinarySearchTree<T, Comp>::BinarySearchTree() {
+inline BinarySearchTree<T, Comp>::BinarySearchTree():root(NULL){
 }
 
 template<typename T, typename Comp>
 inline BinarySearchTree<T, Comp>::BinarySearchTree(
 		const BinarySearchTree& bst) {
+	root = bst.root;
+	cmp = bst.cmp;
 }
 
 template<typename T, typename Comp >
 inline BinarySearchTree<T, Comp>::~BinarySearchTree() {
+}
+
+template<typename T, typename Comp >
+const BinarySearchTree<T, Comp>& BinarySearchTree<T, Comp>::operator=(const BinarySearchTree& bst) {
+	root = bst.root;
+	cmp = bst.cmp;
+	return *this;
+}
+
+template<typename T, typename Comp>
+void BinarySearchTree<T, Comp>::insert(const T& elem) {
+	return insert(elem, root);
+
+}
+
+
+
+template<typename T, typename Comp>
+void BinarySearchTree<T, Comp>::insert(const T& elem, Node* root) {
+	if(NULL == root){
+		root = new Node(elem, NULL, NULL);
+		return ;
+	}
+
+	if(elem <= root->data){
+		return insert(elem, root->lchild);
+	}
+	else{
+		return insert(elem, root->rchild);
+	}
+	return ;
+}
+
+template<typename T, typename Comp>
+inline bool BinarySearchTree<T, Comp>::contains(const T& elem, Node* root) const {
+	if(NULL == root){
+		return false;
+	}
+
+	if(elem == root->data){
+		return true;
+	}
+	else if(elem < root->data){
+		return contains(elem, root->lchild);
+	}
+	else{
+		return contains(elem, root->rchild);
+	}
+	return false;
 }
 
 
